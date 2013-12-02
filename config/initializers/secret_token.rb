50cbc4ff665a36f5ffe8892e4a2647defbfa9bfa
Tom Hanlon <tomhanlon@gmail.com>
2013-12-02 10:07:26 -0600

    initial commit, metube Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metube::Application.config.secret_key_base = '390160cac2e0218a16da026d333553cb65dd4e0e94a6079eb8a1889bf0595722d256e3bfac04bb47f7adf82095fd0208d72894dfd61013ba3b9e2f24d0378c0c'
